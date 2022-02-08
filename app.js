fda1d3a7ab34e586b5415d0a64f0c5de2203208c
mcnultyshane <mcnultyshanej@gmail.com>
2022-02-08 15:31:50 -0500

    designed all tetrominos from the grid

--- app.js
+++ app.js
@@ -12,21 +12,53 @@ const lTetromino = [
     [1, width+1, width*2+1, width*2],
     [0, width, width+1, width+2]
 ]
+const lBackTetromino = [
+    [0, 1, width+1, width*2+1],
+    [width, width+1, width+2, width*2],
+    [1, width+1, width*2+1, width*2+2],
+    [2, width, width+1, width+2]
+]
 
-const oTetrimino= [
+const tTetromino = [
+    [width, width+1, width+2, width*2+1],
+    [1, width, width+1, width*2+2],
+    [1, width, width+1, width+2],
+    [1, width+1, width+2, width*2+2]
+]
+const oTetromino = [
+    [0, 1, width, width+1],
+    [0, 1, width, width+1],
+    [0, 1, width, width+1],
     [0, 1, width, width+1]
 ]
 
-const sTetrimino= [
-
+const sTetromino = [
+    [width+1, width+2, width*2,width*2+1],
+    [0, width, width+1, width*2+1],
+    [width+1, width+2, width*2,width*2+1],
+    [0, width, width+1, width*2+1]
 ]
 
-const tTetrimino= [
-
+const sBackTetromino = [
+    [width, width+1, width*2+1, width*2+2],
+    [2, width+1, width+2, width*2+1],
+    [width, width+1, width*2+1, width*2+2],
+    [2, width+1, width+2, width*2+1]
 ]
 
-const stickTetrimino= [
+const stickTetromino = [
+    [1, width+1, width*2+1, width*3+1],
+    [width, width+1, width+2, width+3],
+    [1, width+1, width*2+1, width*3+1],
+    [width, width+1, width+2, width+3]
+]
+
+const theTetrominos = [lTetromino, lBackTetromino, sTetromino, sBackTetromino, oTetromino, tTetromino, stickTetromino]
     
-]
-    console.log(squares);
+let currentPosition = 4
+let current = theTetrominos[0][0]
+
+
+console.log(theTetrominos);
+
 })
