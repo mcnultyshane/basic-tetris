048619019aca7ae2a43118b404e4876c2bc64716
mcnultyshane <mcnultyshanej@gmail.com>
2022-02-08 15:45:57 -0500

    added functions for drawing and undrawing the shapes to the grid.

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', ()=> {
 
 // the Tetrominoes
 const lTetromino = [
-    [1, width+1, width+2, 2],
+    [1, 2, width+1, width*2+1],
     [width, width+1, width+2, width*2+2],
     [1, width+1, width*2+1, width*2],
     [0, width, width+1, width+2]
@@ -56,8 +56,24 @@ const stickTetromino = [
 const theTetrominos = [lTetromino, lBackTetromino, sTetromino, sBackTetromino, oTetromino, tTetromino, stickTetromino]
     
 let currentPosition = 4
-let current = theTetrominos[0][0]
 
+// randomly select a tetromino and its first rotation
+let random = Math.floor(Math.random()*theTetrominos.length) 
+let current = theTetrominos[random][0]
+
+// draw the first rotation for the first tetromino
+function draw() {
+    current.forEach(index => {
+        squares[currentPosition + index].classList.add('tetromino')
+    })
+}
+
+// undraw the tetromino
+function undraw() {
+    current.forEach(index => {
+        squares[currentPosition + index].classList.remove('tetromino')
+    })
+}
 
 console.log(theTetrominos);
 
