6f5c886fec2a83becbc6430c003ea13a34c15875
mcnultyshane <mcnultyshanej@gmail.com>
2022-02-08 12:17:19 -0500

    adding query selectors to JS.

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', ()=> {
-    const grid= document.querySelector('.grid')
-    let squares = document.querySelectorAll('.')
+    const grid= document.querySelector('.grid') 
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const ScoreDisplay = document.querySelector('#score')
+    const StartBtn = document.querySelector("#start-button")
     const width =10
 
-    
+    console.log(squares);
 })
