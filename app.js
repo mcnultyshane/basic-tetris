5bfb4a7b92c8a82ed49d755d623540e75e1fb958
mcnultyshane <mcnultyshanej@gmail.com>
2022-02-08 12:08:57 -0500

    initial files begun from the inital game tutorial.

---
+++ app.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', ()=> {
+    const grid= document.querySelector('.grid')
+    let squares = document.querySelectorAll('.')
+    const width =10
+
+    
+})
