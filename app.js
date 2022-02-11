3e60954be3b9b0ebd91edfb6f273a1b4defd3bc3
mcnultyshane <mcnultyshanej@gmail.com>
2022-02-11 14:28:39 -0500

    added rotation to tetrominos and making minigrid for the next up visualization.

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ const lBackTetromino = [
     [1, width+1, width*2+1, width*2+2],
     [2, width, width+1, width+2]
 ]
-
 const tTetromino = [
     [width, width+1, width+2, width*2+1],
     [1, width, width+1, width*2+2],
@@ -31,21 +30,18 @@ const oTetromino = [
     [0, 1, width, width+1],
     [0, 1, width, width+1]
 ]
-
 const sTetromino = [
     [width+1, width+2, width*2,width*2+1],
     [0, width, width+1, width*2+1],
     [width+1, width+2, width*2,width*2+1],
     [0, width, width+1, width*2+1]
 ]
-
 const sBackTetromino = [
     [width, width+1, width*2+1, width*2+2],
     [2, width+1, width+2, width*2+1],
     [width, width+1, width*2+1, width*2+2],
     [2, width+1, width+2, width*2+1]
 ]
-
 const stickTetromino = [
     [1, width+1, width*2+1, width*3+1],
     [width, width+1, width+2, width+3],
@@ -56,6 +52,7 @@ const stickTetromino = [
 const theTetrominos = [lTetromino, lBackTetromino, sTetromino, sBackTetromino, oTetromino, tTetromino, stickTetromino]
     
 let currentPosition = 4
+let currentRotation = 0
 
 // randomly select a tetromino and its first rotation
 let random = Math.floor(Math.random()*theTetrominos.length) 
@@ -83,11 +80,11 @@ function control(e) {
     if(e.keyCode === 37) {
         moveLeft()
     } else if (e.keyCode === 38) {
-        // rotate
+        rotate()
     } else if (e.keyCode === 39) {
-        // moveRight()
+        moveRight()
     } else if (e.keyCode === 40) {
-        // moveDown()
+        moveDown()
     }
 }
 document.addEventListener('keyup', control)
@@ -105,7 +102,7 @@ function freeze() {
         current.forEach(index => squares[currentPosition + index].classList.add("taken"))
         // Start a new tetromino falling
         random = Math.floor(Math.random() * theTetrominos.length)
-        current = theTetrominos[random][0]
+        current = theTetrominos[random][currentRotation]
         currentPosition = 4
         draw()
     }
@@ -120,11 +117,33 @@ function moveLeft() {
     if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
         currentPosition +=1
     }
-
     draw()
 }
 
+// move the tetromino right, unless it is at the edge or there is a blockage
+function moveRight() {
+    undraw()
+    const isAtRightEdge = current.some(index => (currentPosition + index) % width === width -1)
 
+    if (!isAtRightEdge) currentPosition +=1
+
+    if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+        currentPosition -=1
+    }
+    draw()
+}
+
+// rotate the tetromino
+function rotate() {
+    undraw()
+    currentRotation ++
+    // if the current rotation gets to 4, make it go back to zero
+    if(currentRotation === current.length) { 
+        currentRotation = 0
+    }
+    current = theTetrominos[random][currentRotation]
+    draw()
+}
 
 
 })
